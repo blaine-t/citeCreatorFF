2c15742d72c52a4316e2a8cafe59008515ed6ed7
Blaine Traudt <blaine-t@bathost.net>
2023-07-02 19:25:34 -0500

    Updated to new copy method

--- background.js
+++ background.js
@@ -1,16 +1,11 @@
-chrome.runtime.onMessage.addListener(
-	function (req, sender, callback) {
-		var rv, el;
-		//console.log(req.text);
-		if (req.text.length > 0) {
-			el = document.createElement('textarea');
-			document.body.appendChild(el);
-			el.value = req.text;
-			el.select();
-
-			rv = document.execCommand("copy");
-
-			document.body.removeChild(el);
-		}
+chrome.runtime.onMessage.addListener(function (req, sender, callback) {
+	if (req.text.length > 0) {
+		navigator.clipboard.writeText(req.text)
+			.then(() => {
+				console.log('Cite copied to clipboard');
+			})
+			.catch((error) => {
+				console.error('Unable to copy cite to clipboard:', error);
+			});
 	}
-);
+});
